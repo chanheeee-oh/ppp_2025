9984469ea567b2734fb1d8de8c0b0278cc28e270
chanheeee-oh <och0126@naver.com>
2025-03-19 16:53:51 +0900

    Add files via upload

---
+++ hw02.py
@@ -0,0 +1,28 @@
+temp_c= 30
+temp_f = (temp_c * 9 / 5) + 32
+print("{}C=>{}F".format(temp_c, temp_f))
+
+temp_c= 0
+temp_f = (temp_c * 9 / 5) + 32
+print("{}C=>{}F".format(temp_c, temp_f))
+
+weight = 60
+height = 170/100
+BMI = 60/(170**2)
+print("BMI",weight, height)
+
+r=4
+pi=3.14
+area=pi*r**2
+print("area",area)
+
+upper=5
+down=3
+high=4
+s=(5+3)*4*0.5
+print("s", s)
+
+product=2000
+discount=2000*0.15
+price=product-discount
+print("prince", price)
